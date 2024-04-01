ffd32df1383220df0569f6c3a612194de02bcbad
PrathamD16 <pratham.doke.1602@gmail.com>
2024-04-02 00:31:51 +0530

    pcap_file process successfull

--- NIDS/NIDSApp/views.py
+++ NIDS/NIDSApp/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 
 # Create your views here.
 
 def processPCAP(request):
     return render(request, 'index.html')
 
+# def scanPCAP(request):
+#     return HttpResponse('Hello World')
+
 def scanPCAP(request):
-    return HttpResponse('Hello World')
+    if request.method == 'POST':
+        if 'pcap_file' not in request.FILES:
+            return HttpResponseBadRequest('No file uploaded!')
+
+        pcap_file = request.FILES['pcap_file']  # Access the uploaded file object
+
+        # Read the file content
+        with pcap_file.open(mode='rb') as f:
+            file_content = f.read()
+
+        # Process the file content as needed (e.g., save it, analyze it)
+        # ... your logic here ...
+
+        return HttpResponse('File content processed successfully!')
+
+    return render(request, 'upload_pcap.html')
